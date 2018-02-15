49cf36a59217e3ef3d58305da523993131624133
Alasdair King <ajk203@cam.ac.uk>
2018-02-15 14:52:22 +0000

    comments on otp

--- server-scripts/CSD3/get_otp.sh
+++ server-scripts/CSD3/get_otp.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -x
+#This script should be called from the json file(Strudle)
+# to fetch the one time password for the client
 
 module load turbovnc
 vncpasswd -o -display localhost:1
